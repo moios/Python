8266d445cd595307986140389c8d50b0967d9eeb
moios <osmarmoises@gmail.com>
2024-08-08 16:43:42 -0400

    operadores logicos

---
+++ logicos.py
@@ -0,0 +1,13 @@
+A = True
+print("Original A: ", A)
+print("Negacion A: ", not A)
+
+print("A       B       A or B")
+A = True; B = True
+print(A,B,A and B)
+A = True; B = False
+print(A,B,A and B)
+A = False; B = True
+print(A,B,A and B)
+A = False; B = False
+print(A,B,A and B)
